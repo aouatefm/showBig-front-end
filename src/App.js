a081706f81c1fb26f7480cd447b0a5a38a2746ab
Aouatef Mareghni <aouatef_mareghni@hotmail.com>
2021-02-23 14:10:54 +0100

    added coursel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import NavbarC from "./components/NavBar/Navbar";
+import Carousel from "./containers/carousel/Carousel";
 
 function App() {
     return (
         <div className="App">
           <NavbarC/>
+            <main role="main">
+                <Carousel />
+            </main>
         </div>
 
     );
