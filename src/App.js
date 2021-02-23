e8e2144a6e05d320900123dcc0df4d205009073f
Aouatef Mareghni <aouatef_mareghni@hotmail.com>
2021-02-23 13:49:30 +0100

    Navbar v2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import {Switch, Route} from 'react-router-dom';
-import Header from "./components/header/header.component";
-import Vendor from "./components/store/store.component";
+import NavbarC from "./components/NavBar/Navbar";
 
 function App() {
     return (
         <div className="App">
-            <header>
-                <Header/>
-            </header>
-            <main>
-                <div id='content-wrap'><Switch>
-                    <Route path='/store-listing' component={Vendor}/>
-                </Switch>
-                </div>
-                {/*<Footer/>*/}
-            </main>
+          <NavbarC/>
         </div>
 
     );
