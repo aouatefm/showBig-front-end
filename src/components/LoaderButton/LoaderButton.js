9c664e901448497b983a36cbf0c31ba65404490c
Aouatef Mareghni <aouatef_mareghni@hotmail.com>
2021-02-24 12:01:17 +0100

    added LoaderButton

---
+++ src/components/LoaderButton/LoaderButton.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button, Glyphicon } from "react-bootstrap";
+import "./LoaderButton.css";
+
+const LoaderButton = ({
+                          isLoading,
+                          className="",
+                          disabled=false,
+                          ...props
+                      }) => {
+    return (
+        <Button
+            className={`LoaderButton ${className}`}
+            disabled={disabled || isLoading}
+            {...props}
+        >
+            {isLoading && <Glyphicon glyph="refresh" className="spinning" />}
+            {props.children}
+        </Button>
+    );
+}
+
+export default LoaderButton;
