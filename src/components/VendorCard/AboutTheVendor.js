93a8f6c48795e205f200ac9274eba19e7a3df734
Aouatef Mareghni <aouatef_mareghni@hotmail.com>
2022-06-19 11:39:59 +0100

    added vendor details

---
+++ src/components/VendorCard/AboutTheVendor.js
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from 'react';
+import VendorService from "../../services/VendorService";
+import VendorCard from "../VendorCard/VendorCard";
+
+function useStoreId(id) {
+    const [store, setStore] = useState(null);
+    useEffect(async () => {
+        const newStore = await VendorService.getVendorById(id);
+        setStore(newStore);
+    }, [])
+    return store
+}
+const AboutTheVendor =  ({store}) => {
+    const vendor = useStoreId(store)
+
+
+
+    return (
+
+        <div>
+            {vendor && <>
+                <p>{store}</p>
+                <p>{vendor.address}</p>
+                <p>{vendor.description}</p>
+                <p>{vendor.is_active}</p>
+
+                <VendorCard vendor={vendor}/>
+            </>}
+
+        </div>
+
+
+    );
+}
+
+export default AboutTheVendor;
